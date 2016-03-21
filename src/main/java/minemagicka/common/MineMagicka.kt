416679f4434b6b79cb3bbb606c738b7da5b10d78
yrsegal <yrsegal@gmail.com>
2016-03-21 18:05:15 -0400

    Commit mod files

---
+++ src/main/java/minemagicka/common/MineMagicka.kt
@@ -0,0 +1,34 @@
+package minemagicka.common
+
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.SidedProxy
+import net.minecraftforge.fml.common.event.FMLInitializationEvent
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
+
+@Mod(modid = "minemagicka", name = "MineMagicka", version = "r1-1")
+class MineMagicka {
+    companion object {
+        @Mod.Instance("minemagicka")
+        lateinit var instance: MineMagicka
+
+//        @SidedProxy(serverSide = "",
+//                clientSide = "")
+//        lateinit var proxy: CommonProxy
+    }
+
+    @Mod.EventHandler
+    fun preInit(event: FMLPreInitializationEvent) {
+//        proxy.pre(event)
+    }
+
+    @Mod.EventHandler
+    fun init(event: FMLInitializationEvent) {
+//        proxy.init(event)
+    }
+
+    @Mod.EventHandler
+    fun post(event: FMLPostInitializationEvent) {
+//        proxy.post(event)
+    }
+}
